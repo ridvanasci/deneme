d3c4257cd15f0bf46cce4b5b47733692fe93b676
ridvan <asci.ridvan@gmail.com>
2025-05-18 16:24:34 +0300

    hello

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select the button and the div
+const button = document.getElementById("myButton");
+const button2 = document.getElementById("myButton2");
+const div1 = document.getElementById("div1");
+
+// Add a click event listener to the button
+button.addEventListener("click", () => {
+    // Update the text content of the div
+    div1.textContent = "Hello World";
+});
+button2.addEventListener("click", () => {
+
+    div2.textContent = `Merhaba ${document.getElementById("myTextarea").value}`;
+    console.log(document.getElementById("myTextarea").value);
+});
